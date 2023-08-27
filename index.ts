ce66a43d253446f80375f84ffcedf80c18b0c158
MosheCohen <mmck345@gmail.com>
2023-08-27 12:14:56 +0300

    add Method Chaining

--- index.ts
+++ index.ts
@@ -84,90 +84,91 @@
 // 4. Method Chaining
 
 
-// class Shape {
-//     info() {
-//         return 'This is a shape'
-//     }
-// }
+class Shape {
+    info() {
+        return 'This is a shape'
+    }
+}
 
-// class Rectangle extends Shape {
-//     width: number;
-//     height: number;
+class Rectangle extends Shape {
+    width: number;
+    height: number;
 
-//     constructor(width: number, height: number) {
-//         super();
-//         this.width = width;
-//         this.height = height;
-//     }
+    constructor(width: number, height: number) {
+        super();
+        this.width = width;
+        this.height = height;
+    }
 
-//     area() {
-//         return this.width * this.height
-//     }
+    area() {
+        return this.width * this.height
+    }
 
-//     info(): string {
-//         return 'This is a Rectangle'
-//     }
+    info(): string {
+        return 'This is a Rectangle'
+    }
  
-//     scale(coefficient: number) {
-//         this.height *= coefficient;
-//         this.width *= coefficient;
-//     }
+    scale(coefficient: number) {
+        this.height *= coefficient;
+        this.width *= coefficient;
+        return this
+    }
 
-//     static mergeRectangle(rec1: Rectangle, rec2: Rectangle) {
-//         return new Rectangle(rec1.width + rec2.width, rec1.height + rec2.height)
-//     }
-// }
+    static mergeRectangle(rec1: Rectangle, rec2: Rectangle) {
+        return new Rectangle(rec1.width + rec2.width, rec1.height + rec2.height)
+    }
+}
 
 // 5. Shape with draw method
 
-class Shape {
-    constructor() {}
+// class Shape {
+//     constructor() {}
 
-    draw() {
-        return 'Drawing a shape'
-    }
-}
+//     draw() {
+//         return 'Drawing a shape'
+//     }
+// }
 
-class Triangle extends Shape {
-    constructor() {
-        super()
-    }
+// class Triangle extends Shape {
+//     constructor() {
+//         super()
+//     }
 
-    draw() {
-        return 'Drawing a Triangle'
-    }
-}
+//     draw() {
+//         return 'Drawing a Triangle'
+//     }
+// }
 
-class Circle extends Shape {
-    constructor() {
-        super()
-    }
+// class Circle extends Shape {
+//     constructor() {
+//         super()
+//     }
 
-    draw() {
-        return 'Drawing a Circle'
-    }
-}
+//     draw() {
+//         return 'Drawing a Circle'
+//     }
+// }
 
-class Square extends Shape {
-    constructor() {
-        super()
-    }
+// class Square extends Shape {
+//     constructor() {
+//         super()
+//     }
 
-    draw() {
-        return 'Drawing a Square'
-    }
-}
+//     draw() {
+//         return 'Drawing a Square'
+//     }
+// }
 
-function renderShapes(items: Array<Triangle | Circle | Square | Shape>) {
-    items.forEach((shape => {
-        console.log(shape.draw());
+// function renderShapes(items: Array<Triangle | Circle | Square | Shape>) {
+//     items.forEach((shape => {
+//         console.log(shape.draw());
         
-    }))
-}
+//     }))
+// }
 
-// const tr = new Triangle()
-// const cr = new Square()
-// const sh = new Shape()
+// // const tr = new Triangle()
+// // const cr = new Square()
+// // const sh = new Shape()
 
-// renderShapes([tr, cr, sh])
+// // renderShapes([tr, cr, sh])
 
